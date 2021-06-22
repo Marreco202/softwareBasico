c0e7b99fc621309fd0b60775190f63ec348f20ee
Marreco202 <slygodinho@gmail.com>
2021-06-22 12:15:39 -0300

    files

---
+++ main.c
@@ -0,0 +1,28 @@
+/*Felipe Maia Meiga 2011460 3WA*/
+/*João Victor Godinho Woitschach 2011401 3WB*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "gera.h"
+
+int main() {
+  FILE *myfp;
+  funcp funcaoSimples;
+  int res;
+
+  /* Abre o arquivo fonte */
+  if ((myfp = fopen("teste.txt", "r")) == NULL) {
+    perror("Falha na abertura do arquivo fonte\n");
+    exit(1);
+  }
+  /* traduz a função Simples */
+  funcaoSimples = gera(myfp);
+  fclose(myfp);
+
+  /* chama a função */
+  res = (*funcaoSimples) (9, 8, 7);  /* passando argumentos apropriados */
+
+  printf("%d\n", res);
+
+  libera(funcaoSimples);
+}
